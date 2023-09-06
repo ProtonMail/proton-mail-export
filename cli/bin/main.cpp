25f216eb1e82e3140c1106112dc4796f20c32caf
Leander Beernaert <leander.beernaert@proton.ch>
2023-09-06 09:09:17 +0200

    feat(GODT-2894): Add TUI UI for CLI tool

--- cli/bin/main.cpp
+++ cli/bin/main.cpp
@@ -15,9 +15,59 @@
 // You should have received a copy of the GNU General Public License
 // along with Proton Export Tool.  If not, see <https://www.gnu.org/licenses/>.
 
-#include <iostream>
-#include "etcpp.hpp"
+#include <string>
+
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/component_options.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/dom/elements.hpp>
+
+#include <etconfig.hpp>
+#include <etcpp.hpp>
 
 int main() {
+    auto session = etcpp::Session(et::DEFAULT_API_URL);
+
+    std::string email;
+    std::string password;
+    auto inputEmail = ftxui::Input(&email, "Email");
+
+    auto screen = ftxui::ScreenInteractive::TerminalOutput();
+    auto exit = screen.ExitLoopClosure();
+
+    auto passwordOptions = ftxui::InputOption{};
+    passwordOptions.password = true;
+    auto inputPassword = ftxui::Input(&password, "password", passwordOptions);
+    auto loginButton = ftxui::Button("Login", [&]() {
+        try {
+            auto loginState = session.login(email.c_str(), password.c_str());
+            if (loginState != etcpp::Session::LoginState::LoggedIn) {
+                std::cerr << "Login requires mores steps" << std::endl;
+            }
+        } catch (const etcpp::Exception& e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+        }
+        exit();
+    });
+
+    auto components = ftxui::Container::Vertical({
+        inputEmail,
+        inputPassword,
+        loginButton,
+    });
+
+    auto renderer = ftxui::Renderer(components, [&] {
+        return ftxui::vbox({
+                   ftxui::text("Login into your proton account"),
+                   ftxui::separator(),
+                   ftxui::hbox(ftxui::text("Email   : "), inputEmail->Render()),
+                   ftxui::hbox(ftxui::text("Password: "), inputPassword->Render()),
+                   ftxui::separator(),
+                   loginButton->Render(),
+               }) |
+               ftxui::border;
+    });
+
+    screen.Loop(renderer);
     return EXIT_SUCCESS;
 }
